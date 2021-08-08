8ac3e893c89a50a85412950138c56c7e17890977
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-08-08 19:29:56 +0200

    Project completed

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 USER = password.USER
-
 Spotify_ID = password.Client_ID
 Spotify_password = password.Client_Secret
 
+# Input user date
 while True:
     date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD\n")
     try:
@@ -19,45 +19,42 @@ while True:
     else:
         break
 
+# searching track on www.billboard.com
 URL = "https://www.billboard.com/charts/hot-100/"
 URL_and_date = URL + date
-
-print(URL_and_date)
 
 response = requests.get(URL_and_date)
 billboard_web_page = response.text
 
 soup = BeautifulSoup(billboard_web_page, "html.parser")
-
 all_music = soup.find_all(name="span", class_="chart-element__information__song text--truncate color--primary")
-print(all_music)
 
 all_music_list = [song.get_text() for song in all_music]
 
-print(all_music_list)
-
+# Login to Spotify
 scope = "playlist-modify-public"
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=USER))
 
+# Getting year from user date
 year = date.split("-")[0]
-print(year)
 
+# Creating list URI of searching music
 track_URI_list = []
-
 for music in all_music_list:
     q = f"track: {music} year: {year}"
     search_track = sp.search(q=q, limit=1, type="track")
-    print(search_track)
-    try:
+
+    if not search_track["tracks"]["items"]:
+        continue
+
+    else:
         track_uri = search_track["tracks"]["items"][0]["uri"]
         track_URI_list.append(track_uri)
-        print(track_uri)
-    except IndexError:
-        print("NONE")
 
 
+# Creating empty playlist on Spotify
 new_playlist = sp.user_playlist_create(user=USER, name=f"{date} Billboard 100", public=False)
 new_playlist_ID = new_playlist["id"]
 
+# Adding list URI of music to playlist
 sp.playlist_add_items(playlist_id=new_playlist_ID, items=track_URI_list, position=None)
